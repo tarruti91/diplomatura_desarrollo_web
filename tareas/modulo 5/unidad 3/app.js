5567e6edb12ff375d0ea4e459681b3ed4bd05e7f
Tomas Arruti <tomas_arruti@hotmail.com>
2023-01-08 17:21:32 -0300

    tarea modulo 5 unidad 3

---
+++ tareas/modulo 5/unidad 3/app.js
@@ -0,0 +1,75 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+require ('dotenv').config();
+var pool = require('./models/bd');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+//select
+//pool.query('select * from empleados').then(function(resultados){console.log(resultados)
+//});
+
+//insert
+//var obj = {
+//  nombre:'Juan',
+//  apellido:'Lopez',
+//  trabajo: 'docente',
+//  edad: '38',
+//  salario: '1500',
+//  mail: 'juanlopez@gmail.com',
+//}
+
+//pool.query ('insert into empleados set ?',[obj]).then(function(resultados){console.log(resultados)
+//});
+
+//update
+//var id= 1;
+//var obj = {
+//  nombre: 'Pablo',
+//  apellido: 'Gomez' 
+//}
+//pool.query ('update empleados set ? where idempleado= ?', [obj, id]). then (function (resultados){ console.log(resultados);
+//})
+
+//borrar
+//var id = 24;
+//pool.query('delete from empleados where idempleado= ?',[id]).then (function(resultados){console.log(resultados);
+//})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
