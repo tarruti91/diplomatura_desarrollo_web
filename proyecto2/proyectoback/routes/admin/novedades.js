fbbcd706c7fcfeb1ff0fb41d4effffe55312fbdd
Tomas Arruti <tomas_arruti@hotmail.com>
2023-01-22 02:06:04 -0300

    carga, modifica y elimina novedades

--- proyecto2/proyectoback/routes/admin/novedades.js
+++ proyecto2/proyectoback/routes/admin/novedades.js
@@ -1,10 +1,81 @@
+const { Router } = require('express');
 var express = require('express');
+const pool = require('../../models/bd');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
-router.get ('/', async function (req, res, next){res.render('admin/novedades', {
-    layout: 'admin/layout',
-    usuario: req.session.nombre
+router.get('/', async function (req, res, next) {
+    var novedades = await novedadesModel.getNovedades();
+    res.render('admin/novedades', {
+        layout: 'admin/layout',
+        usuario: req.session.nombre,
+        novedades
+    });
 });
+
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    })
 });
+
+router.post('/agregar', async (req, res, next) => {
+    try {
+        if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+            await novedadesModel.insertNovedades(req.body);
+            res.redirect('/admin/novedades')
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'Todos los campos son obligatorios'
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargo la novedad'
+        })
+    }
+})
+
+router.get('/eliminar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect('/admin/novedades');
+})
+
+router.get('/modificar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    //console.log(req.params.id);
+    var novedad = await novedadesModel.getNovedadesById(id);
+
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        novedad
+    })
+})
+router.post('/modificar', async (req, res, next) => {
+    try {
+        var obj = {
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo
+        }
+        console.log(obj)
+
+        await novedadesModel.modificarNovedadById(obj, req.body.id);
+        res.redirect('/admin/novedades');
+    } catch (error) {
+        console.log(error)
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se modifico la novedad'
+        })
+    }
+})
 
 module.exports = router;
